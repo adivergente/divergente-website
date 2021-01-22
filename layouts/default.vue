3fe952c9cb16e02457362cd91c449dd8b4f02b46
martinalanis <martinalanis.dev@gmail.com>
2021-01-21 20:36:13 -0600

    init project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <!-- <LayoutHeader :light-mode="headerLightMode"/> -->
+    <v-main>
+      <nuxt />
+    </v-main>
+    <!-- <LayoutFooter/> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="home">
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 900px;
+  background: url('~assets/img/img01.webp')no-repeat center;
+  background-size: cover;
+}
+</style>
